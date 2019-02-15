b8fad986a3db2c10bb1d4d1b056a662e67ac55bd
unknown <kparker4089@myhta.org>
2019-02-15 09:05:39 -1000

    completed loops

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
-/* Done in wednesday class:
+ /* Done in wednesday class:
 1. What is a loop?
-
+A function that repeats itself. 
 2. What is the syntax of a loop?
 for(i = 0; i < 5; i++){
-
+for(initializing; terminating; incrementing){
+    //tasks to be repeated
 }
-initializing
-terminating
-incrementing
+}
+initializing = start at? ex. var i = 0
+terminating = end at?, ex. i > 10
+incrementing = how often/periods, intervals in between, ex i = i + 1
+
+i = i + 1
+shortcut: i++
+i = i + 2
+shortcut: i+=2
+
+for(i = 0; i < 5; i++){
+    console.log("I am smart.")
+}
+1- I am smart. i = 0
+2- I am smart i = 1
+3- I am smart i = 2
+4- I am smart. i = 3
+5- I am smart i = 4
+6 -- i = 5 X
 
 3. Give a few examples.
 */
 
+//Create a loop that prints "HTA rocks." 3 times
+for( var i = 0; i < 3; i = i + 1){
+    console.log("HTA rocks." )
+}
+for(var i= 0; i < 10; i =i+2){
+    console.log(i)
+}
+//var i = i+1;
+// Create a loop that prints even numbers.
 //4. Create a loop that prints numbers from 0-10.
 
 //5. Create a loop that prints odd numbers from 1-9.
